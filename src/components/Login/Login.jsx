ea94dfe704385acb04698c3993694c1d217cb09f
Ignacio Fleming <ignacioflemings@gmail.com>
2024-03-28 17:30:32 -0300

    applied style to login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,25 +2,34 @@ import styles from "./Login.module.css";
 import Grid from "@mui/material/Unstable_Grid2";
 import TextField from "@mui/material/TextField";
 import { Box, Button, Typography } from "@mui/material";
+
+const formContainer = {
+  display: "flex",
+  flexDirection: "column",
+  width: "80%",
+  height: "100%",
+  justifyContent: "center",
+};
+
 const Login = () => {
   return (
     <div className={styles.container}>
       <Grid container sx={{ height: "100%" }}>
         <Grid md sx={{ display: "flex", alignItems: "stretch", justifyContent: "center" }}>
-          <img src="https://res.cloudinary.com/dah7yxmc5/image/upload/v1711495777/habitacion-vacia-sillas-escritorios_23-2149008873_ydrfj5.jpg" alt="background-image" />
+          <img className={styles.mainImg} src="https://res.cloudinary.com/dah7yxmc5/image/upload/v1711495777/habitacion-vacia-sillas-escritorios_23-2149008873_ydrfj5.jpg" alt="background-image" />
         </Grid>
-        <Grid md>
-          <Box>
-            <img src="https://res.cloudinary.com/dah7yxmc5/image/upload/v1680611686/logo_ecommerce_ci16kw.png" alt="Logo.png" />
-            <Typography variant="h1" color="initial">
+        <Grid md sx={{ display: "flex", alignItems: "stretch", justifyContent: "center" }}>
+          <Box sx={formContainer}>
+            <img className={styles.logo} src="https://res.cloudinary.com/dah7yxmc5/image/upload/v1680611686/logo_ecommerce_ci16kw.png" alt="Logo.png" />
+            <Typography variant="h2" color="initial" mb={1} fontWeight="semibold">
               Login
             </Typography>
-            <Typography variant="subtitle1" color="initial">
+            <Typography variant="subtitle1" color="initial" mb={6}>
               Wellcome Back. Please login to your account.
             </Typography>
             <form id="login" className={styles.form}>
-              <TextField id="email" label="Email" variant="outlined" />
-              <TextField id="password" label="Password" variant="outlined" type="password" />
+              <TextField id="email" label="Email" variant="outlined" fullWidth />
+              <TextField id="password" label="Password" variant="outlined" type="password" fullWidth />
               <Typography variant="body1" color="initial">
                 Forgot my password.
               </Typography>
@@ -28,13 +37,11 @@ const Login = () => {
                 SIGN IN
               </Button>
             </form>
-          </Box>
-          <Box>
+
             <Typography variant="subtitle2" color="initial" fontSize={15}>
               OR
             </Typography>
-          </Box>
-          <Box>
+
             <Typography variant="h6" color="initial">
               Sign In with Google
             </Typography>
